0cc68ef3f2dc75af50bb866808cc4dd8ee76590f
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2020-08-18 12:34:47 -0300

    modularizando o codigo

---
+++ models/produtos.go
@@ -0,0 +1,48 @@
+package models
+
+import (
+	"../db"
+)
+
+type Produto struct {
+	Id              int
+	Nome, Descricao string
+	Preco           float64
+	Quantidade      int
+}
+
+func BuscaTodosProdutos() []Produto {
+	db := db.ConectaComBancoDeDados()
+	selectDeTodosOsProdutos, err := db.Query("select * from produtos")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	p := Produto{}
+	produtos := []Produto{}
+
+	for selectDeTodosOsProdutos.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err = selectDeTodosOsProdutos.Scan(&id, &nome, &descricao, &preco, &quantidade)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		p.Nome = nome
+		p.Descricao = descricao
+		p.Preco = preco
+		p.Quantidade = quantidade
+
+		produtos = append(produtos, p)
+
+	}
+
+	// defer serve para fechar conexao do banco de dados
+	defer db.Close()
+
+	return produtos
+}
