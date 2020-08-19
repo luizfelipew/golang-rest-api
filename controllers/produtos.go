501ad72c7f208d0a08fedff4c8b23bdec96307ae
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2020-08-19 10:34:12 -0300

    terminando de modularizar o codigo

---
+++ controllers/produtos.go
@@ -0,0 +1,15 @@
+package controllers
+
+import (
+	"html/template"
+	"net/http"
+
+	"../models"
+)
+
+var temp = template.Must(template.ParseGlob("templates/*.html"))
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	todosOsProdutos := models.BuscaTodosProdutos()
+	temp.ExecuteTemplate(w, "Index", todosOsProdutos)
+}
--- main.go
+++ main.go
@@ -1,21 +1,12 @@
 package main
 
 import (
-	"html/template"
 	"net/http"
 
-	"./models"
+	"./routes"
 )
 
-var temp = template.Must(template.ParseGlob("templates/*.html"))
-
 func main() {
-	http.HandleFunc("/", index)
+	routes.CarregaRotas()
 	http.ListenAndServe(":8000", nil)
-}
-
-func index(w http.ResponseWriter, r *http.Request) {
-	todosOsProdutos := models.BuscaTodosProdutos()
-	temp.ExecuteTemplate(w, "Index", todosOsProdutos)
-
 }
