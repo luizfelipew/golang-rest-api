8465ac378dc9f1e1a03dba5acf9a5a4a3c425e72
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2020-09-11 11:15:15 -0300

    Atualizando produto

--- models/produtos.go
+++ models/produtos.go
@@ -13,7 +13,7 @@ type Produto struct {
 
 func BuscaTodosProdutos() []Produto {
 	db := db.ConectaComBancoDeDados()
-	selectDeTodosOsProdutos, err := db.Query("select * from produtos")
+	selectDeTodosOsProdutos, err := db.Query("select * from produtos order by id asc")
 	if err != nil {
 		panic(err.Error())
 	}
@@ -88,11 +88,12 @@ func EditaProduto(id string) Produto {
 		var nome, descricao string
 		var preco float64
 
-		err = produtoDoBanco.Scan(&id, &nome, &descricao, &quantidade, &preco)
+		err = produtoDoBanco.Scan(&id, &nome, &descricao, &preco, &quantidade)
 		if err != nil {
 			panic(err.Error())
 		}
 
+		produtoParaAtualizar.Id = id
 		produtoParaAtualizar.Nome = nome
 		produtoParaAtualizar.Descricao = descricao
 		produtoParaAtualizar.Preco = preco
@@ -100,4 +101,15 @@ func EditaProduto(id string) Produto {
 	}
 	defer db.Close()
 	return produtoParaAtualizar
+}
+
+func AtualizaProduto(id int, nome, descricao string, preco float64, quantidade int) {
+	db := db.ConectaComBancoDeDados()
+
+	AtualizaProduto, err := db.Prepare("update produtos set nome=$1, descricao=$2, preco=$3, quantidade=$4 where id=$5")
+	if err != nil {
+		panic(err.Error())
+	}
+	AtualizaProduto.Exec(nome, descricao, preco, quantidade, id)
+	defer db.Close()
 }
